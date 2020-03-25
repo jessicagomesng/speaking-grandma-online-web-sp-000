a2fe2f04c34e35b6cef8dac0e7780ed886e04005
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-25 12:02:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase != phrase.upcase()
-    "I LOVE YOU TOO PUMPKIN!"
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   elsif !phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   end

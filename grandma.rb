e5d6816b77252e6aca5f2cfb67cf5020b020524e
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-25 12:02:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@ def speak_to_grandma(phrase)
   end
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif !phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 

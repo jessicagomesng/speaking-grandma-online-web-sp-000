09d9e23a5b02a4e20a18d69babbc7d9b928431c3
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-25 11:56:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif !phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
